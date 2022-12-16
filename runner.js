28a226dbd17333ff0d3cf103b001aae2804f26ed
Vasily <ololol2113@mail.ru>
2022-12-17 02:23:41 +0300

    generRandForGorizontalString

--- runner.js
+++ runner.js
@@ -6,8 +6,10 @@ const readAndSolve = require('./readAndSolve');
 
 // Все судоку для решения доступны в файле puzzles.txt.
 // Прочесть файл puzzles.txt в кодировке 'utf-8' и передать его содержимое в функцию readAndSolve.
-fs.readFile(
+const read = fs.readFile(
   './puzzles.txt',
   'utf-8',
   readAndSolve,
 );
+
+module.exports = read;
--- sudoku.js
+++ sudoku.js
@@ -1,10 +1,28 @@
+const fs = require('fs');
+
+const read = require('./runner');
+
+const boardString = read;
+
 /**
  * Принимает игровое поле в формате строки — как в файле sudoku-puzzles.txt.
  * Возвращает игровое поле после попытки его решить.
  * Договорись со своей командой, в каком формате возвращать этот результат.
  */
+
+
 function solve(boardString) {
+
+  let arr = boardString.split('');
+  let newArr = [];
+  for (let i = 0; i < arr.length; i += 9) {
+    if (arr[i] === 9) {
+
+newArr.push(arr[i] )
+    }
+  }
 }
+console.log(solve(1 - 58 - 2----9--764 - 52--4--819 - 19--73 - 6762 - 83 - 9----- 61 - 5-- - 76-- - 3 - 43--2 - 5 - 16--3 - 89--))
 
 /**
  * Принимает игровое поле в том формате, в котором его вернули из функции solve.
@@ -13,7 +31,7 @@ function solve(boardString) {
 function isSolved(board) {
 
 }
- 
+
 /**
  * Принимает игровое поле в том формате, в котором его вернули из функции solve.
  * Возвращает строку с игровым полем для последующего вывода в консоль.
